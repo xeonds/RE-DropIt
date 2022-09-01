d1e8e2debaa12fc075d861be2865b362144267bf
jiujiu <1505855642@qq.com>
2022-09-01 19:47:26 +0800

    修改了一些bug

--- demo/main.py
+++ demo/main.py
@@ -11,32 +11,30 @@ import os
 class Rule:
     def __init__(self) -> None:     # 配置加载
         self.config_path='./config.json'
+        self.config={"default":{"jpg": {"action": "move","param":"./"}}}
         if not os.path.isfile(self.config_path):
-            with open(self.config_path, 'w') as f:
-                f.write('{"default":{"jpg": {"action": "move","param":"./"}}}')
-        self.config=json.load(open(self.config_path,mode='r'))
+            self.save()
+        else:
+            self.config=json.load(open(self.config_path, 'r'))
     def save(self):                 # 保存配置
         with open(self.config_path,mode='w') as config:
             config.write(json.dumps(self.config))
     def add_rule(self, conf, rule): # 添加配置
-        self.config[conf].append(rule)
+        self.config[conf] = rule
         self.save()
-
 
 # 文件处理类
 class Handler:
     def __init__(self,rules): # 加载配置
         self.rules = rules
-    def __fun_selector(self,filename):  # 根据后缀选择文件处理函数
+    def fun_selector(self,filename):  # 根据后缀选择文件处理函数
         subname = filename.split('.')[-1]
         if subname in self.rules.keys():
-            self.rules[subname](filename)
-    def process(self,path): # 遍历处理文件夹下所有文件
-        (dirpath, dirname,filename) = os.walk(path)
-        for file in filename:
-            self.__fun_selector(file)
-        for dir in dirpath: # 递归处理子文件夹
-            self.process(dir)
+            globals()[self.rules[subname]['action']](filename, self.rules[subname]['param'])
+    def process(self,path): # 递归遍历处理文件夹下所有文件
+        for root,dirs,files in os.walk(path):
+            for file in files:
+                self.fun_selector(root+'\\'+file)
 
 if __name__ == '__main__':
     conf = Rule()   # 初始化规则管理器
@@ -46,20 +44,25 @@ if __name__ == '__main__':
     c = input('select a config(enter for default):')
     # 菜单：添加规则/开始处理文件
     s = input('s for start process,a for add rule:')
+
     if s == 's':
         while True:
             # 将待处理文件拽到窗口上添加路径，一行一个路径
             path = input()
-            if path == '\n':    # 回车结束录入路径
+            if path == '':    # 回车结束录入路径
                 break
             file_list.append(path)
         # 用配置文件初始化文件处理器，并调用process进行处理
-        Handler(conf.config['default' if c=='\n' else c]).process(file_list)
+        for i in file_list:
+            if os.path.isdir(i):
+                Handler(conf.config['default' if c=='' else c]).process(i)
+            if os.path.isfile(i):
+                Handler(conf.config['default' if c=='' else c]).fun_selector(i)
     elif s == 'a':
         # 一条规则包括文件后缀名，以及对应的处理函数和参数
         rule={}
         subname=input('input subname(like txt,pdf):')
         rule[subname] = {
-            'action':input('input rule(move, copy, ...):'),'param':input('input rule(move, copy, ...):')
+            'action':input('input rule(move, copy, ...):'),'param':input('input param:')
         }
-        conf.add_rule('default' if c=='\n' else c, rule)
+        conf.add_rule('default' if c=='' else c, rule)
