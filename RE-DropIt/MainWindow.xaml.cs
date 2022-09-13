d58e33697c1a1606ad540b9decc91695a113e0d8
xeonds <1456506329@qq.com>
2022-09-13 13:26:59 +0800

    添加WPF项目文件

---
+++ RE-DropIt/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace RE_DropIt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_folder_select_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog
+            {
+                Description = "Time to select a folder",
+                UseDescriptionForTitle = true,
+                SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)
+        + Path.DirectorySeparatorChar,
+                ShowNewFolderButton = true
+            };
+
+            if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show("打开文件夹失败", "错误");
+            }
+            listBox.ItemsSource = Directory.GetFiles(dialog.SelectedPath);
+        }
+    }
+
+    class DirectoryRecord
+    {
+        public DirectoryInfo Info { get; set; }
+
+        public IEnumerable<FileInfo> Files
+        {
+            get
+            {
+                return Info.GetFiles();
+            }
+        }
+
+        public IEnumerable<DirectoryRecord> Directories
+        {
+            get
+            {
+                return from di in Info.GetDirectories("*", SearchOption.TopDirectoryOnly)
+                       select new DirectoryRecord { Info = di };
+            }
+        }
+    }
+}
