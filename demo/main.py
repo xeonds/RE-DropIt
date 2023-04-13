54b1096d3fcddceb9e62aa0c37c69985cc1769a6
xeonds <1456506329@qq.com>
2023-04-13 16:31:15 +0800

    Add extract function & introduction of program

--- demo/main.py
+++ demo/main.py
@@ -22,8 +22,10 @@ class App:
         self.file_list = tk.Listbox(self.master)
         self.file_list.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
 
-        self.process_button = tk.Button(self.master, text="Process", command=self.process_folders)
-        self.process_button.pack(side=tk.BOTTOM, padx=5, pady=5)
+        self.process_button = tk.Button(self.master, text="Process", command=lambda: self.process_folders(mode = "process"))
+        self.process_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.extract_button = tk.Button(self.master, text="Extract", command=lambda: self.process_folders(mode = "extract"))
+        self.extract_button.pack(side=tk.LEFT, padx=5, pady=5)
 
         # Align buttons and label horizontally
         self.file_list_label.pack(side=tk.TOP, anchor=tk.W)
@@ -38,15 +40,13 @@ class App:
         # Add "Show folders after processed" checkbox
         self.show_folders_var = tk.BooleanVar()
         self.show_folders_var.set(False)
-        def toggle_show_folders():
-            pass # TODO: Implement function to toggle show_folders_var
         options_menu = tk.Menu(menubar)
-        options_menu.add_checkbutton(label="Show folders after processed", variable=self.show_folders_var, command=toggle_show_folders)
+        options_menu.add_checkbutton(label="Show folders after processed", variable=self.show_folders_var)
         menubar.add_cascade(label="Options", menu=options_menu)
 
         # Add "About" menu item
         def show_about():
-            messagebox.showinfo("About", "This is a file processing app.")
+            messagebox.showinfo("About", "This is a file processing app.\n Process means sort all files by extension.\n Extract means move all files to the root dir.")
         menubar.add_command(label="About", command=show_about)
 
     def add_folder(self):
@@ -60,40 +60,53 @@ class App:
     def remove_folder(self):
         self.file_list.delete(tk.END)
 
-    def process_folders(self):
+    def process_folders(self, mode):
         processed_folders = []
         for directory in self.file_list.get(0, tk.END):
             directory_path = directory.split(" - ")[-1]
-            if process_directory(directory_path):
-                self.file_list.delete(self.file_list.get(0, tk.END).index(directory))
-                processed_folders.append(directory_path)
-                if self.show_folders_var.get():
-                    os.startfile(directory_path)
+            process_directory(directory_path, mode)
+            self.file_list.delete(self.file_list.get(0, tk.END).index(directory))
+            processed_folders.append(directory_path)
+            if self.show_folders_var.get():
+                os.startfile(directory_path)
         messagebox.showinfo("Processing Complete", "All selected folders have been processed.")
 
-    def show_folders(self, folders):
-        pass
+class process_directory:
+    def __init__(self, directory, mode, parent_dir=None):
+        self.directory = directory
+        self.mode = mode
+        self.parent_dir = directory
+        self.file_dict = {}
+        self.run()
 
-def process_directory(directory, parent_dir=None):
-    if parent_dir is None:
-        parent_dir = directory
-    file_dict = {}
-    for filename in os.listdir(directory):
-        extension = os.path.splitext(filename)[1]
-        if os.path.isdir(os.path.join(directory, filename)):
-            process_directory(os.path.join(directory, filename), parent_dir)
-        elif extension:
-            if extension not in file_dict:
-                file_dict[extension] = []
-            file_dict[extension].append(filename)
-    for extension, filenames in file_dict.items():
-        temp_path = os.path.join(parent_dir, extension[1:])
-        os.makedirs(temp_path, exist_ok=True)
-        for filename in filenames:
-            old_path = os.path.join(directory, filename)
-            new_path = os.path.join(temp_path, filename)
-            shutil.move(old_path, new_path)
-    return True
+    def run(self):
+        self.scan_dir()
+        if self.mode == "process":
+            for extension, filenames in self.file_dict.items():
+                temp_path = os.path.join(self.directory, extension[1:])
+                os.makedirs(temp_path, exist_ok=True)
+                for file in filenames:
+                    old_path = os.path.join(self.directory, file['path'])
+                    new_path = os.path.join(temp_path, file['name'])
+                    shutil.move(old_path, new_path)
+        elif self.mode == "extract":
+            for _, filenames in self.file_dict.items():
+                for file in filenames:
+                    old_path = os.path.join(self.directory, file['path'])
+                    new_path = os.path.join(self.parent_dir, file['name'])
+                    shutil.move(old_path, new_path)
+        else:
+            print("Invalid mode selected")
+
+    def scan_dir(self):
+        for root, dirs, files in os.walk(self.directory):
+            for file in files:
+                extension = os.path.splitext(file)[1]
+                if extension:
+                    if extension not in self.file_dict:
+                        self.file_dict[extension] = []
+                    self.file_dict[extension].append({'name': file, 'path': os.path.join(root, file)})
+
 
 root = tk.Tk()
 app = App(root)
